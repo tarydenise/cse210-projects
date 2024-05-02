333583ce838da1589bc151028d7c6f3ec47b203f
tarydenise <tarydenise@gmail.com>
2024-05-02 03:53:21 -0700

    Completed Preps 1 and 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,70 @@
 using System;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+        
+        Console.Write("What is your grade percentage? ");
+        string userValue = Console.ReadLine();
+        int percentage = int.Parse(userValue);
+
+        string letter = "";
+        if (percentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (percentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (percentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (percentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        int remainder =  percentage % 10;
+        string sign = "";
+        if (remainder >= 7)
+        {
+            sign = "+";
+        }
+        else if (percentage >= 100)
+        {
+            sign = "+";
+        }
+        else if (remainder < 3)
+        {
+            sign = "-";
+        }
+        else if (percentage < 54)
+        {
+            sign = "-";
+        }
+        else
+        {
+            sign = "";
+        }
+
+        Console.WriteLine($"Current grade: {letter}" + sign);
+
+        if (percentage > 70)
+        {
+            Console.WriteLine("Congratulations, you passed the course!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, you did not pass this time. Better luck next time!");
+        }
     }
 }
